7abf8725e0e820546c354f6100c8bafbe1266fbf
dmytrenkov-cience <vladyslav.dmytrenko@cience.com>
2021-09-29 00:35:03 +0300

    default template

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createStorages } from "vuex";
 
-createApp(App).mount('#app')
+const storage = createStorages({
+ state() {
+  return {};
+ },
+});
+
+app.use(storage);
+
+const app = createApp(App);
+
+app.mount("#app");
